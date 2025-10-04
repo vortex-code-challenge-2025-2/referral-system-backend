26790fa808172d995ca08021d92db8d712896f2f
João Pedro Lima <joaopedrocal90@gmail.com>
2025-10-04 18:12:48 -0300

    feat: profile route done

---
+++ src/middlewares/auth.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      console.error(error);
+      return res.status(401).json({ error: "Unauthorized, invalid token." });
+    }
+  }
+
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized, no token provided." });
+  }
+};
+
+export default protect;
